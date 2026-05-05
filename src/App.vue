c7fb523f1db8746500530e93b8b6ae1f91b6ba6a
nambelaas <salmaniseif@gmail.com>
2026-05-05 15:08:35 +0700

    Add arguments event and validation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
-<script setup lang="ts">
+<script lang="ts">
 import { ref } from 'vue'
 import Alert from './components/Alert.vue';
 import Alerts from './components/Alerts.vue';
+import LoginForm from './components/LoginForm.vue';
 
-const items = ref(3)
-const show = ref(true)
-const alert = ref({
-  message: "Message",
-  type: ["success", "warning", "danger"]
-})
+// const items = ref(3)
+// const show = ref(true)
+// const alert = ref({
+//   message: "Message",
+//   type: ["success", "warning", "danger"]
+// })
+
+export default {
+  name: 'App',
+  components: {
+    Alert,
+    Alerts,
+    LoginForm
+  },
+  props:{
+    items: {
+      type: Number,
+      default: 0
+    },
+    show:{
+      type: Boolean,
+      default: true
+    },
+    alert: {
+      type: Object,
+      default: () => ({
+        message: "Message",
+        type: ["success", "warning", "danger"]
+      })
+    }
+  },
+  methods:{
+    handleSubmit(email: String, password: String): void{
+      alert(email + " " + password)
+    }
+  }
+}
 
 </script>
 
 <template>
   <!-- <Alert :message="items+' item has been removed'" type="success" :show="true"/> -->
-  <Alert :message="items+' item has been removed'" type="danger" :show="show" @alert-close="show = false" />
+  <!-- <Alert :message="items+' item has been removed'" type="danger" :show="show" @alert-close="show = false" /> -->
   <!-- <Alerts :message="items+' item has been removed'" type="errors" :types="alert.type"/> -->
+   <LoginForm @submit="handleSubmit" />
+
 </template>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1>Login</h1>
+    <form @submit.prevent="$emit('submit', email, password)">
+        <div class="mb-3">
+            <input type="email" v-model="email" class="form-control" placeholder="Email">
+        </div>
+        <div class="mb-3">
+            <input type="password" v-model="password" class="form-control" placeholder="Password">
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</template>
+
+<script lang="ts">
+export default {
+    name: "LoginForm",
+    data: () =>({
+        email: "",
+        password: ""
+    }),
+    emits: {
+        submit: (email: String, password: String) => {
+            if (email && password) {
+                return true
+            } else{
+                console.warn("Invalid submit event: email and password are required")
+                return false
+            }
+        }
+    }
+}
+</script>
